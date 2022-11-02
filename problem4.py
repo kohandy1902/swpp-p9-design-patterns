c00ede1efd343ad450b227982c04fb8415f20a12
Donghyun Koh <andy1902@snu.ac.kr>
2022-11-02 20:13:47 +0900

    Solve P4

--- problem4.py
+++ problem4.py
@@ -7,10 +7,14 @@ class CompositeFunction(Function):
     def __init__(self, *functions):
         # TODO: fill this code
         # NOTE: `functions` is a tuple of arguments
+        self.function = functions
 
     def __call__(self, value):
         # TODO: fill this code
         # HINT: `reversed(TUPLE)` returns a tuple in reversed order
+        for f in reversed(self.function):
+            value = f(value)
+        return value
 
 
 class F(Function):
@@ -20,7 +24,7 @@ class F(Function):
 
 class G(Function):
     def __call__(self, value):
-        return value ** 2
+        return value**2
 
 
 class H(Function):
